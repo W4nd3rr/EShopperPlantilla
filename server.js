ff27242a66cb0c4f68fa6d7d07b26bcc075508e0
unknown <w4nd3rr>
2026-03-03 13:38:15 -0400

    fix

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
-const express = require('express');
-const path = require('path');
-
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
 const app = express();
 
 // Carpeta pública
 app.use(express.static(path.join(__dirname, 'public')));
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename)
 
 // Ruta principal
 app.get('/', (req, res) => {
